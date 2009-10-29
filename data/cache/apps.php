966292d51b378b0b7793f5e9955e6f52cdbfd787
Guoliang Cao <gcao@vonage.com>
2009-10-29 10:46:25 -0400

    First commit

---
+++ data/cache/apps.php
@@ -0,0 +1,49 @@
+<?php
+$_CACHE['apps'] = array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'DISCUZ',
+    'name' => 'Discuz!',
+    'url' => 'http://localhost/bbs/index.php',
+    'authkey' => 'A3Ia5bb2b5C4leccm9Wbv1vbH4RaI8acPe31m0N2q9e2Y6F6G9u5H9D8AbB5F9Ce',
+    'ip' => '',
+    'viewprourl' => '',
+    'apifilename' => 'uc.php',
+    'charset' => 'utf-8',
+    'dbcharset' => 'utf8',
+    'synlogin' => '1',
+    'recvnote' => '1',
+    'extra' => '',
+    'tagtemplates' => '<?xml version="1.0" encoding="ISO-8859-1"?>
+<root>
+ <item id="template"><![CDATA[<a href="{url}" target="_blank">{subject}</a>]]></item>
+ <item id="fields">
+ <item id="subject"><![CDATA[标题]]></item>
+ <item id="uid"><![CDATA[用户 ID]]></item>
+ <item id="username"><![CDATA[发帖者]]></item>
+ <item id="dateline"><![CDATA[日期]]></item>
+ <item id="url"><![CDATA[主题地址]]></item>
+ </item>
+</root>',
+  ),
+  2 => 
+  array (
+    'appid' => '2',
+    'type' => 'DISCUZ',
+    'name' => '论坛',
+    'url' => 'http://localhost/bbs',
+    'authkey' => 'C7f7jf09e94dv9M1H36aydNd4dfco3i8EdW3w1D8A9S0V5h4k7l2waWaM8H0M5Q8',
+    'ip' => '',
+    'viewprourl' => '',
+    'apifilename' => 'uc.php',
+    'charset' => 'utf-8',
+    'dbcharset' => 'utf8',
+    'synlogin' => '1',
+    'recvnote' => '1',
+    'extra' => '',
+    'tagtemplates' => '<?xml version="1.0" encoding="ISO-8859-1"?><root> <template><![CDATA[<a href="{url}" target="_blank">{subject}</a>]]></template> <fields> <subject><![CDATA[标题]]></subject> <uid><![CDATA[用户 ID]]></uid> <username><![CDATA[发帖者]]></username> <dateline><![CDATA[日期]]></dateline> <url><![CDATA[主题地址]]></url> </fields></root>',
+  ),
+);
+
+?>
---
+++ data/logs/200910.php
@@ -0,0 +1,3 @@
+<?PHP exit;?>	UCenterAdministrator	unknown	1256248652	login	succeed
+<?PHP exit;?>	UCenterAdministrator	unknown	1256248668	user_add	username=admin
+<?PHP exit;?>	UCenterAdministrator	unknown	1256248683	admin_add	username=admin
